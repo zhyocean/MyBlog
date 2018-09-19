794ab13b47b7c072c32394f2747696fd248a34bc
zhyocean <1125694337@qq.com>
2018-09-19 13:19:35 +0800

    init project

---
+++ src/main/java/com/zhy/MyBlogApplication.java
@@ -0,0 +1,12 @@
+package com.zhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogApplication.class, args);
+	}
+}
